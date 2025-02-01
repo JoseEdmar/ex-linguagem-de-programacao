2784e8bd3d34f6033d66d1dd5527ef1af8de0a26
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-01 20:54:54 -0300

    exercício 6

---
+++ ex 6 pag 38.py
@@ -0,0 +1,12 @@
+'''6. Escreva um programa que receba dois números, calcule e apresente o resultado do
+primeiro número elevado ao segundo.'''
+
+# Recebe os números
+num1 = float(input("Dígite um número\n"))
+num2 = float(input("Dígite outro número\n"))
+
+# Eleva o 1º número ao 2º
+elevado = pow(num1, num2)
+
+# Imprime o resultado
+print(num1, "Elevado há", num2, "é:", elevado)
