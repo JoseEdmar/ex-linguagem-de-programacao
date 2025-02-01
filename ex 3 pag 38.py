20b6a71cd7955ab3966d3c109e52841d4fb3c1ec
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-01 20:12:22 -0300

    adicionando comentários no código

--- ex 3 pag 38.py
+++ ex 3 pag 38.py
@@ -1,10 +1,13 @@
 '''3. Escreva um programa que leia um número inteiro e apresente seu antecessor e
 seu sucessor.'''
 
+# Recebe o número digitado
 num = int(input("Dígite um número\n"))
 
+# Faz o calculo do número sucessor e antecessor
 numero_sucessor = num + 1
 numero_antecessor = num - 1
 
+# Exibe o resultado na tela
 print("O número antessesor10git init é:", numero_antecessor, "o número dígitado foi:", num, "o número sucessor é:", numero_sucessor)
 
---
+++ ex 4 pag 38.py
@@ -0,0 +1,11 @@
+'''4. Elabore um programa que receba quatro notas e calcule a média aritmética entre
+elas.'''
+
+nota_1 = float(input("Dígite uma nota"))
+nota_2 = float(input("Dígite uma nota"))
+nota_3 = float(input("Dígite uma nota"))
+nota_4 = float(input("Dígite uma nota"))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print("A média entre as notas é:", media)
