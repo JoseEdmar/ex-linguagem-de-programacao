75a24bfc13348f8cd1331eba80458e642882891c
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-01 20:04:21 -0300

    primeiro exercício de escrever códigos

---
+++ ex 3 pag 38.py
@@ -0,0 +1,10 @@
+'''3. Escreva um programa que leia um número inteiro e apresente seu antecessor e
+seu sucessor.'''
+
+num = int(input("Dígite um número\n"))
+
+numero_sucessor = num + 1
+numero_antecessor = num - 1
+
+print("O número antessesor10git init é:", numero_antecessor, "o número dígitado foi:", num, "o número sucessor é:", numero_sucessor)
+
