a91b8c92bfb6fd4b9a3d4e5fca74f4345117a081
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-01 20:18:39 -0300

    colocando comentários no código pag 38 ex 4

--- ex 4 pag 38.py
+++ ex 4 pag 38.py
@@ -1,11 +1,14 @@
 '''4. Elabore um programa que receba quatro notas e calcule a média aritmética entre
 elas.'''
 
+# Recebendo as notas
 nota_1 = float(input("Dígite uma nota"))
 nota_2 = float(input("Dígite uma nota"))
 nota_3 = float(input("Dígite uma nota"))
 nota_4 = float(input("Dígite uma nota"))
 
+# Calcula a média
 media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
 
+# Imprime a média
 print("A média entre as notas é:", media)
