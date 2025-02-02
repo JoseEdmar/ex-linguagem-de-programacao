4befe18d4aa6d3822aef7dc4eafbae9ebf8a456b
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-02 12:38:15 -0300

    Terminado o exercício 8

---
+++ Página 38/Exercício 8.py
@@ -0,0 +1,8 @@
+'''8. Escreva um programa que leia o nome de uma pessoa e imprima a seguinte mensagem,
+na tela: “Bem-vindo(a) à disciplina de Lógica de Programação e Algoritmos,
+<nome>”; onde o campo <nome> deve ser substituído pelo nome informado pelo
+usuário.'''
+
+nome = str(input("Qual é o seu nome?\n"))
+
+print("Bem-vindo(a) à disciplina de Lógica de Programação e Algoritmos", nome)
--- Página 38/README.md
+++ Página 38/README.md
@@ -21,3 +21,9 @@ primeiro número elevado ao segundo.
 
 Exercício 7:
 Elabore um programa que calcula a área de um trapézio.
+
+Exercício 8:
+Escreva um programa que leia o nome de uma pessoa e imprima a seguinte mensagem,
+na tela: “Bem-vindo(a) à disciplina de Lógica de Programação e Algoritmos,
+<nome>”; onde o campo <nome> deve ser substituído pelo nome informado pelo
+usuário.
