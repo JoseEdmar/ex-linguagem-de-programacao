7a3adaa1af4f5fc4bbf97fc0c50ecbad31bfc6ae
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-03 20:43:35 -0300

    Finalizado✅

---
+++ Página 38/Exercício 11.py
@@ -0,0 +1,16 @@
+'''11. Escreva um programa que, dado o raio de um círculo, calcule a sua área e o perímetro.
+A área é a superfície do objeto, dada por 3,14 multiplicados pelo raio elevado ao
+quadrado. O perímetro é a medida do contorno do objeto dado por 2 multiplicados
+por 3,14, multiplicados, novamente, pelo raio.'''
+
+# Recebe o raio
+raio = float(input("informe o raio\n"))
+
+# Calculos conforme informado no exercício
+area = 3.14 *raio **2
+perimetro = 2 * 3.14 * raio
+
+# Imprime o resultado
+print("a area é", area)
+print("o perimetro é:", perimetro)
+
