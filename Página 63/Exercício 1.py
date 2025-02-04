6b2f210150c5de511a8eead702c6ca012f8d6c3f
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-04 10:55:47 -0300

    Finalizado✅

--- Página 63/Exercício 1.py
+++ Página 63/Exercício 1.py
@@ -1,1 +1,10 @@
 # 1. Faça um programa que leia um número e informe se ele é divisível por cinco.
+
+# Recebe o número
+num = float(input("Dígite um número: "))
+
+# Verifica divisibilidade por 5
+if num % 5 == 0:
+  print("o número é divisível por 5")
+else:
+  print("O número não é divisível por 5")
