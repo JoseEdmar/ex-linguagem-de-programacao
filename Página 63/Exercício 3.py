c9295ff31afa1116a3efe971b7181ca8fbbd4224
José Edmar <joseedmarc.gouveia@gmail.com>
2025-02-05 13:55:32 -0300

    Terminado o exercício 3

---
+++ Página 63/Exercício 3.py
@@ -0,0 +1,12 @@
+'''3. Construa um programa que receba a idade de uma pessoa e identifique sua classe
+eleitoral: não eleitor (menor que 16 anos de idade), eleitor obrigatório (entre 18 e
+65 anos) e eleitor facultativo (entre 16 e 18 e maior que 65).'''
+
+idade = int(input("Qual a sua idade?"))
+
+if idade < 16:
+  print("Não eleitor")
+elif idade >= 18 and idade <= 65:
+  print("Eleitor obrigatório")
+elif idade >= 16 and (idade < 18 or idade > 65):
+  print("Eleitor Facultativo")
