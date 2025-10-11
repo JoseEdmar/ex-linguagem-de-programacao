2ebe1c08806a75358991b05f191820d8c1c3cc67
José Edmar Constantino <109236899+JoseEdmar@users.noreply.github.com>
2025-10-11 23:29:23 +0000

    Finalizado✅

--- Página 115/Exercício 2.py
+++ Página 115/Exercício 2.py
@@ -1,17 +1,20 @@
+'''Faça um programa que leia duas listas A e B, ambas com 20 números inteiros. Efetue 
+a soma entre as duas listas em uma terceira lista C e imprima C em ordem crescente.'''
 lista_A = []
 lista_B = []
 
 
 print("Dígite 20 números para a lista A")
 for i in range(1, 21):
-    numero = int(input(f"Dígite o {i}° número da lista A\n"))
-    lista_A.append(numero)
+    numero1 = int(input(f"Dígite o {i}° número da lista A\n"))
+    lista_A.append(numero1)
 
 print("Dígite 20 números para a lista B")
 for i in range(1, 21):
-    numero = int(input(f"Dígite o {i}° número da lista B\n"))
-    lista_B.append(numero)
+    numero2 = int(input(f"Dígite o {i}° número da lista B\n"))
+    lista_B.append(numero2)
 
 lista_C = [a + b for a, b in zip(lista_A, lista_B)]
+lista_C.sort()
 
-print(f"A lista reversa é: {lista_C}")
+print(f"Lista ordenada e somada: {lista_C}")
---
+++ Página 115/README.md
@@ -0,0 +1,6 @@
+1. Escreva um programa que leia uma lista com um total de 30 elementos numéricos 
+do tipo inteiro. Em seguida, apresente a mesma lista impressa em ordem inversa 
+à ordem de entrada.
+
+2. Faça um programa que leia duas listas A e B, ambas com 20 números inteiros. Efetue 
+a soma entre as duas listas em uma terceira lista C e imprima C em ordem crescente.
